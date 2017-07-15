4b34183544abb2db65f914cbfc5f090cf6694cbc
1352noComment <raisa.kabir@u.nus.edu>
2017-07-16 03:04:30 +0800

    Add files via upload
    Change code for get id for profile page

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,20 +4,18 @@ var User = require('../models/user');
 var Profile = require('../models/profile');
 
 
-router.get('/:id', isLoggedIn, function(req, res, next) {
+router.get('/:id',isLoggedIn, function(req, res, next) {
   Profile.find(function(err, profile) {
     var productChunks = [];
     var chunkSize = 3;
     for (var i = 0; i < profile.length; i += chunkSize) {
-      for (var r=0; r< profile[i].friends.length;r++) {
-        if ((profile[i].friends[r].equals(req.user._id)) || profile[i].mode===true){
-          productChunks.push(profile);
-        }
+        productChunks.push(profile.slice(i, i + chunkSize));
       }
-    }
+
     res.render('profile/profile', {  profiles: productChunks });
   });
 });
+
 
 router.get('/:id/scheduler', isLoggedIn, function(req, res){
   res.render('scheduler');
@@ -62,51 +60,57 @@ router.post('/:id/add', isLoggedIn, function(req, res) {
 
 
 
-router.get('/edit/:id', isLoggedIn,function(req, res) {
-  Profile.findById(req.params.id, function(err, profile){
-    if (!err)
+  router.get('/edit/:id', isLoggedIn,function(req, res) {
+    Profile.findById(req.params.id, function(err, profile){
+      if (!err)
       res.render('profile/profile_edit', {profiles :profile});
+    });
   });
-});
 
-  //get
-  router.get('/:id', isLoggedIn, function(req, res) {
+  // Update Submit POST Route
+  router.post('/edit/:id', function(req, res){
     Profile.findById(req.params.id, function(err, profile){
       if (err)
       {
         res.status(404).send('Sorry, page not found');
       }
+
       else {
-        User.findById(profile.user, function(err, user){
-          for (var i = 0; i < profile.length; i += chunkSize) {
-            for (var r=0; r< profile[i].friends.length;r++) {
+        let profile = {};
+        profile.title = req.body.title;
+        profile.description = req.body.description;
+        profile.mode = req.body.mode;
 
-              if (profile[i].friends[r].equals(req.user._id))
-                res.render('profile/profile');
-            }
+        let query = {_id:req.params.id}
+
+        Profile.update(query, profile, function(err){
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success', 'Mindmap Updated');
+            res.redirect('/profile');
           }
-          req.flash('danger', 'Not Authorized');
-          res.redirect('/');
-
         });
       }
     });
   });
 
 
+
   router.get('/delete/:id', isLoggedIn,function(req, res) {
     Profile.findById(req.params.id, function(err, profile){
       if (!err)
-        res.render('profile/profile_delete', {profiles: profile});
+      res.render('profile/profile_delete', {profiles: profile});
     });
   });
 
 
 
-  router.post('/delete/:id', function(req, res){
+  router.post('/delete/:id',  isLoggedIn,function(req, res){
     Profile.findById(req.params.id, function(err, profile){
       if (err)
-        res.status(404).send("page not found");
+      res.status(404).send("page not found");
 
       if(!(profile.user.equals(req.user._id))){
         res.status(500).send('No authorisation');
@@ -126,7 +130,7 @@ router.get('/edit/:id', isLoggedIn,function(req, res) {
   });
 
 
-  router.post('/invitation/:id', function(req, res){
+  router.post('/invitation/:id',  isLoggedIn,function(req, res){
     Profile.findById(req.params.id, function(err, profile){
       if(!(profile.user.equals(req.user._id))){
         res.status(500).send('Wrong');
@@ -134,7 +138,7 @@ router.get('/edit/:id', isLoggedIn,function(req, res) {
       else {
         User.findOne({'email': req.body.friend}).exec( function(err,user) {
           if (err)
-            console.log(err);
+          console.log(err);
           else {
             profile.friends.push(user.id);
             profile.save();
@@ -147,30 +151,37 @@ router.get('/edit/:id', isLoggedIn,function(req, res) {
   });
 
 
-  router.get('/mindmaps/:id', function(req, res){
+  router.get('/mindmaps/:id', isLoggedIn, function(req, res){
+    var access= false;
     Profile.findById(req.params.id, function(err, profile){
       if (err)
-        res.status(404).send("page not found");
-
-      if(!(profile.user.equals(req.user._id))){
-        res.status(500).send('No authorisation');
+      res.status(404).send("page not found");
+else {
+      for (var i =0; i<profile.friends.length ;i++) {
+        if ((profile.friends[i].equals(req.user._id)) || profile.mode===true){
+          access =true;
+          break;
+        }
       }
-
-      else {
-        res.render('mindmap', {
-          layout: 'mindmap-layout'
-        });
-      }});
-  });
-
-
-    function isLoggedIn(req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      req.session.oldUrl = req.url;
-      req.flash('error_msg','You are not logged in');
-      res.redirect('/users/login');
     }
+    if (access===false)
+    res.status(500).send('No authorisation');
 
-    module.exports = router;
+    else {
+      res.render('mindmap', {
+        layout: 'mindmap-layout'
+      });
+    }
+  });
+});
+
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.session.oldUrl = req.url;
+    req.flash('error_msg','You are not logged in');
+    res.redirect('/users/login');
+  }
+
+  module.exports = router;
